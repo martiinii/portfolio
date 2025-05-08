85be93030983d2e3296cf68dbe588ac869cb68d6
Martin <me.martiinii@gmail.com>
2025-05-08 22:35:10 +0200

    feat: external links to companies projects (#4)

--- components/projects/projects.data.tsx
+++ components/projects/projects.data.tsx
@@ -4,14 +4,14 @@ export type ProjectLink = {
 };
 type Project = {
   title: string;
-  description: string;
+  description: React.ReactNode;
   links?: ProjectLink[];
 };
 export const projects: Project[] = [
   {
     title: "UltraStar",
     description:
-      "Automatically fetches lyrics, YouTube videos, and metadata for 25,000+ songs—so your next party can skip the setup and dive straight into the off-key magic. Built to handle everything behind the scenes, it's the tool that turned my friends' and my weekends into full-blown karaoke parties.",
+      "Automatically fetches lyrics, YouTube videos, and metadata for 25,000+ songs, so your next party can skip the setup and dive straight into the off-key magic. Built to handle everything behind the scenes, it's the tool that turned my friends' and my weekends into full-blown karaoke parties.",
     links: [
       {
         type: "github",
